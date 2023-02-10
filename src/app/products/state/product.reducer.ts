af54bbdefc0074b573bb6852c5211282279f17cd
muttukk <muttukk@gmail.com>
2023-02-09 22:51:34 -0500

    1) Added ngstore devtools 2)Added strongly typed state

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,8 +1,27 @@
 import { createAction, createReducer, on } from "@ngrx/store";
+import * as AppState from '../../state/app.state';
+import { Product } from "../product";
 
-export const productReducer=createReducer(
-    {showProductCode:true}, // initial state
-    on(createAction('[Product] toggle product code'),state=>{
+export interface State extends AppState.State{
+    products:ProductState;
+}
+
+export interface ProductState{
+    showProductCode:boolean;
+    currentProduct:Product;
+    products:Product[];
+}
+
+const initialState:ProductState={
+    showProductCode:true,
+    currentProduct:null,
+    products:[]
+}
+
+export const productReducer=createReducer<ProductState>(
+    //{showProductCode:true} as ProductState, // initial state
+    initialState,
+    on(createAction('[Product] toggle product code'),(state) : ProductState =>{
         //console.log('original State' + JSON.stringify(state));
         return{
             ...state,
